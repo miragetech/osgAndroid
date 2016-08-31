2d6375faaccd709813ee6e9a36e67f45d20b341c
CKehl <Christian.Kehl@uni.no>
2016-08-31 17:19:18 +0200

    Changes to be committed: 	modified:   jni/JNIUtils.h
    Added Interpolate() function

--- org.openscenegraph.android/jni/JNIUtils.h
+++ org.openscenegraph.android/jni/JNIUtils.h
@@ -43,3 +43,47 @@ typedef ReferencedType<osg::Vec2> RefVec2;
 typedef ReferencedType<osg::Vec3> RefVec3;
 typedef ReferencedType<osg::Vec4> RefVec4;
 typedef ReferencedType<osg::Quat> RefQuat;
+
+inline osg::Vec4 Interpolate(osg::Vec2 _texcoord, osg::Image* _img)
+{
+	osg::Vec4 kernel[3][3];
+	int l_k, l_l;
+	float width = _img->s(), height = _img->t();
+	float xc = _texcoord.x()*(width-1); float yc = (1.0-_texcoord.y())*(height-1);
+	int xd = int(floor(xc)); int yd = int(floor(yc));
+
+	//collect values
+	for(int k = (xd-1); k < (xd+2); k++)
+	{
+		for(int l = (yd-1); l < (yd+2); l++)
+		{
+			l_k = k - (xd-1); l_l = l - (yd-1);
+			if((k<0)||(k>int(width-1))||(l<0)||(l>int(height-1)))
+			{
+				kernel[l_k][l_l] = osg::Vec4(0,0,0,0);
+			}
+			else
+			{
+				kernel[l_k][l_l] = _img->getColor(k,l,0);
+			}
+		}
+	}
+
+	float final_colour_r=0, final_colour_g=0, final_colour_b=0, final_colour_a = 0;
+	float x_offset, y_offset;
+	x_offset = (xc - (float)xd) - 0.5;
+	y_offset = (yc - (float)yd) - 0.5;
+	double dist = 0.0;
+	for(int k = 0; k < 3; k++)
+	{
+		for(int l = 0; l < 3; l++)
+		{
+			dist = (sqrt((abs((k-1)+x_offset) * abs((k-1)+x_offset)) + (abs((l-1)+y_offset) * abs((l-1)+y_offset))))/2.121320344;
+			final_colour_r += (1-dist) * kernel[k][l].x();
+			final_colour_g += (1-dist) * kernel[k][l].y();
+			final_colour_b += (1-dist) * kernel[k][l].z();
+			final_colour_a += (1-dist) * kernel[k][l].w();
+		}
+	}
+	return osg::Vec4(final_colour_r/9.0, final_colour_g/9.0, final_colour_b/9.0, final_colour_a/9.0);
+}
