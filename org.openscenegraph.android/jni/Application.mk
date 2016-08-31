c178f04b2b86ec889917b3d17d2d7e9f90a41d59
CKehl <Christian.Kehl@uni.no>
2016-08-31 15:06:20 +0200

    added new compilation files that include 3rdParty libraries and that works for GLES1 and GLES2

--- org.openscenegraph.android/jni/Application.mk
+++ org.openscenegraph.android/jni/Application.mk
@@ -1,12 +1,14 @@
 #ANDROID APPLICATION MAKEFILE
 APP_BUILD_SCRIPT := $(call my-dir)/Android.mk
+NDK_MODULE_PATH := $(call my-dir)
 #APP_PROJECT_PATH := $(call my-dir)
 
 APP_OPTIM := release
 
 APP_PLATFORM 	:= android-8
 APP_STL 	:= gnustl_shared
+APP_CFLAGS      += -fexceptions
 APP_CPPFLAGS 	:= -fexceptions -frtti
-APP_ABI 	:= armeabi
-APP_MODULES     := jniosg-gles1 jniosg-gles2
+APP_ABI 	:= armeabi armeabi-v7a
+#APP_MODULES     := jniosg-gles1 jniosg-gles2
 NDK_TOOLCHAIN_VERSION := 4.9
