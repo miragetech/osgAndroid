3f75043782c68102862581080151d2bf9789f0d6
CKehl <Christian.Kehl@uni.no>
2016-10-19 18:12:00 +0200

    tested and running compilation, ready to be pulled. comment on the changes: the problem why the viewer didn't work was due to the new 3rdParty library setup, which now appears to work like a charm. An expected error will be raised ONCE with the OpenSceneGraph head, as it lacks the following functions:
            inline void set( value_type x, value_type y ) { _v[0]=x; _v[1]=y; }
            inline void set( const Vec2f& rhs) { _v[0]=rhs._v[0]; _v[1]=rhs._v[1]; }
    (to be found in <OSG_TOP>/include/Vec2f)
    
    this needs to be patched (currently: manually).
    
     Changes to be committed:
    	modified:   org.openscenegraph.android/jni/Android.mk
    	modified:   org.openscenegraph.android/jni/Application.mk
    	modified:   org.openscenegraph.osgsimple/src/org/openscenegraph/osgsimple/OSGActivity.java

--- org.openscenegraph.android/jni/Android.mk
+++ org.openscenegraph.android/jni/Android.mk
@@ -1,12 +1,12 @@
 LOCAL_PATH := $(call my-dir)
  
 OSG_SRC_FILES := \
-JNIosgViewer.cpp \
 JNIosg.cpp \
 JNIosgDB.cpp \
 JNIosgUtil.cpp \
 JNIosgGA.cpp \
 JNIUtils.cpp \
+JNIosgViewer.cpp \
 MultiViewNode.cpp \
 GLES2ShaderGenVisitor.cpp \
 screenview.cpp
@@ -16,13 +16,10 @@ OSG_LDLIBS := \
 -losgdb_ive \
 -losgdb_rgb \
 -losgdb_bmp \
--losgdb_tga \
--losgdb_stl \
--losgdb_obj \
--losgdb_dxf \
--losgdb_curl \
 -losgdb_gif \
 -losgdb_jpeg \
+-losgdb_tga \
+-losgdb_obj \
 -losgdb_openflight \
 -losgdb_serializers_osgvolume \
 -losgdb_serializers_osgtext \
@@ -70,73 +67,26 @@ OSG_LDLIBS := \
 -ltiff \
 -ljpeg \
 -lgif \
--lpng \
--lcurl \
--lzlib
-
-
-### Static preparation
-OSG_HEAD:=<path-to-osg-gles1-sdk> 
-OSG_SDK:=$(OSG_HEAD)/$(TARGET_ARCH_ABI)
-OSG_3RDPARTY_DIR:=obj/local/$(TARGET_ARCH_ABI)
-OSG_3RDPARTY_LIBS_DIR:=
-OSG_3RDPARTY_COMPONENTS:=jpeg gif png tiff zlib curl
-
-define add_osg_3rdparty_component
-  include $(CLEAR_VARS)
-  LOCAL_MODULE:=$1
-  ifeq ($(strip $1),gif)
-    LOCAL_SRC_FILES:=$(OSG_HEAD)/$1lib/$(OSG_3RDPARTY_DIR)/lib$1.a
-    OSG_3RDPARTY_LIBS_DIR +=$(OSG_HEAD)/$1lib/$(OSG_3RDPARTY_DIR) 
-  else
-    ifeq ($(strip $1),png)
-      LOCAL_SRC_FILES:=$(OSG_HEAD)/lib$1/$(OSG_3RDPARTY_DIR)/lib$1.a
-      OSG_3RDPARTY_LIBS_DIR +=$(OSG_HEAD)/lib$1/$(OSG_3RDPARTY_DIR) 
-    else
-      ifeq ($(strip $1),tiff)
-        LOCAL_SRC_FILES:=$(OSG_HEAD)/lib$1/$(OSG_3RDPARTY_DIR)/lib$1.a
-        OSG_3RDPARTY_LIBS_DIR +=$(OSG_HEAD)/lib$1/$(OSG_3RDPARTY_DIR) 
-      else
-        ifeq ($(strip $1),jpeg)
-          LOCAL_SRC_FILES:=$(OSG_HEAD)/lib$1/$(OSG_3RDPARTY_DIR)/lib$1.a
-          OSG_3RDPARTY_LIBS_DIR +=$(OSG_HEAD)/lib$1/$(OSG_3RDPARTY_DIR) 
-        else
-          LOCAL_SRC_FILES:=$(OSG_HEAD)/$1/$(OSG_3RDPARTY_DIR)/lib$1.a
-          OSG_3RDPARTY_LIBS_DIR +=$(OSG_HEAD)/$1/$(OSG_3RDPARTY_DIR) 
-        endif
-      endif
-    endif
-  endif
-  include $(PREBUILT_STATIC_LIBRARY)
-endef
-
-$(foreach module,$(OSG_3RDPARTY_COMPONENTS),$(eval $(call add_osg_3rdparty_component,$(module))))
-LOCAL_INCLUDE:=${LOCAL_C_INCLUDES}
+-lpng
 
 ### GLES1 build
 include $(CLEAR_VARS)
-OSG_HEAD:=<path-to-osg-gles1-sdk>
-OSG_SDK:=${OSG_HEAD}/$(TARGET_ARCH_ABI)
-
-define add_lib_dir
-    LOCAL_LDFLAGS += -L$1
-endef
-
+#OSG_HEAD:=<path-to-osg-gles1-sdk>/$(TARGET_ARCH_ABI)
+OSG_SDK := /media/christian/DATA/android-osg-sdk/gles1/$(TARGET_ARCH_ABI)
+OSG_SDK_LIB_PATH:=$(OSG_SDK)/lib
+OSG_SDK_PLUGIN_PATH:=$(OSG_SDK_LIB_PATH)/osgPlugins-3.3.8
 ifneq (,$(wildcard $(OSG_SDK)/include/osg/Config))
 
     APP_MODULES       := jniosg-gles1
-    OSG_SDK_LIB_PATH:=$(OSG_SDK)/lib
-    OSG_SDK_PLUGIN_PATH:=$(OSG_SDK_LIB_PATH)/osgPlugins-3.3.8
     LOCAL_CFLAGS      := -Werror -fno-short-enums -fPIC
     LOCAL_CPPFLAGS    := -DOSG_LIBRARY_STATIC 
     LOCAL_SRC_FILES   := $(OSG_SRC_FILES)
     LOCAL_MODULE      := libjniosg-gles1
-    LOCAL_LDLIBS      := -llog -lGLESv1_CM -ldl -lm
-    LOCAL_WHOLE_STATIC_LIBRARIES+=$(OSG_3RDPARTY_COMPONENTS)
-    LOCAL_C_INCLUDES  := ${LOCAL_INCLUDE} $(OSG_SDK)/include
+    LOCAL_LDLIBS      := -llog -lGLESv1_CM -ldl# -lm
+    LOCAL_C_INCLUDES  := $(OSG_SDK)/include
+    LOCAL_CPP_INCLUDES := $(OSG_SDK)/include
     TARGET_LDLIBS     := $(OSG_LDLIBS)
     LOCAL_LDFLAGS := -L$(OSG_SDK_LIB_PATH) -L$(OSG_SDK_PLUGIN_PATH) 
-    $(foreach path,$(OSG_3RDPARTY_LIBS_DIR),$(eval $(call add_lib_dir,$(path))))
     include $(BUILD_SHARED_LIBRARY)
 else
     $(warning Unable to find osg/Config file in the headers, not building libjniosg-gles1 module)
@@ -144,24 +94,21 @@ endif
 
 ### GLES2 build
 include $(CLEAR_VARS)
-OSG_HEAD:=<path-to-osg-gles2-sdk>
-OSG_SDK2:=${OSG_HEAD}/$(TARGET_ARCH_ABI)
-
+#OSG_SDK := <path-to-osg-gles2-sdk>/$(TARGET_ARCH_ABI)
+OSG_SDK := /media/christian/DATA/android-osg-sdk/gles2/$(TARGET_ARCH_ABI)
+OSG_SDK_LIB_PATH:=$(OSG_SDK)/lib
+OSG_SDK_PLUGIN_PATH := $(OSG_SDK_LIB_PATH)/osgPlugins-3.3.8
 ifneq ( ,$(wildcard $(OSG_SDK)/include/osg/Config))
-    APP_MODULES       := jniosg-gles2
-    OSG_SDK2_LIB_PATH:=$(OSG_SDK2)/lib
-    OSG_SDK2_PLUGIN_PATH:=$(OSG_SDK2_LIB_PATH)/osgPlugins-3.3.8
+    APP_MODULES       += jniosg-gles2
     LOCAL_CFLAGS      := -Werror -fno-short-enums -fPIC
     LOCAL_CPPFLAGS    := -DOSG_LIBRARY_STATIC 
     LOCAL_SRC_FILES   := $(OSG_SRC_FILES)
     LOCAL_MODULE      := libjniosg-gles2
-    LOCAL_LDLIBS      := -llog -lGLESv2 -ldl -lm 
-    LOCAL_WHOLE_STATIC_LIBRARIES+=$(OSG_3RDPARTY_COMPONENTS)
+    LOCAL_LDLIBS      := -llog -lGLESv2 -ldl# -lm
     LOCAL_C_INCLUDES  := $(OSG_SDK)/include
-    LOCAL_C_INCLUDES:=${LOCAL_INCLUDE} $(OSG_SDK2)/include
-    TARGET_LDLIBS := $(OSG_LDLIBS)
-    LOCAL_LDFLAGS  := -L$(OSG_SDK2_LIB_PATH) -L$(OSG_SDK2_PLUGIN_PATH)
-    $(foreach path,$(OSG_3RDPARTY_LIBS_DIR),$(eval $(call add_lib_dir,$(path))))
+    LOCAL_CPP_INCLUDES := $(OSG_SDK)/include
+    TARGET_LDLIBS     := $(OSG_LDLIBS)
+    LOCAL_LDFLAGS     := -L$(OSG_SDK_LIB_PATH) -L$(OSG_SDK_PLUGIN_PATH) 
     include $(BUILD_SHARED_LIBRARY)
 else
     $(warning Unable to find osg/Config file in the headers, not building libjniosg-gles2 module)
--- org.openscenegraph.android/jni/Application.mk
+++ org.openscenegraph.android/jni/Application.mk
@@ -5,10 +5,9 @@ NDK_MODULE_PATH := $(call my-dir)
 
 APP_OPTIM := release
 
-APP_PLATFORM 	:= android-15
-APP_CFLAGS += -fexceptions
-APP_STL 	:= gnustl_shared
-APP_CFLAGS      += -fexceptions
+APP_PLATFORM 	:= android-8
+APP_STL         := gnustl_shared
+APP_CFLAGS      := -fexceptions
 APP_CPPFLAGS 	:= -fexceptions -frtti
 APP_ABI 	:= armeabi armeabi-v7a
 #APP_MODULES     := jniosg-gles1 jniosg-gles2
